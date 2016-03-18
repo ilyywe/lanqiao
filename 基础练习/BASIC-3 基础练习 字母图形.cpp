f88f2fe77af8a7c1a90959c114b93a55bc3fdfd2
陈欣 <95323362@qq.com>
2016-03-18 20:13:11 +0800

    -

---
+++ 基础练习/BASIC-3 基础练习 字母图形.cpp
@@ -0,0 +1,47 @@
+BASIC-3 基础练习 字母图形  
+时间限制：1.0s   内存限制：256.0MB
+提交此题   锦囊1   锦囊2
+问题描述
+利用字母可以组成一些美丽的图形，下面给出了一个例子：
+
+ABCDEFG
+
+BABCDEF
+
+CBABCDE
+
+DCBABCD
+
+EDCBABC
+
+这是一个5行7列的图形，请找出这个图形的规律，并输出一个n行m列的图形。
+
+输入格式
+输入一行，包含两个整数n和m，分别表示你要输出的图形的行数的列数。
+输出格式
+输出n行，每个m个字符，为你的图形。
+样例输入
+5 7
+样例输出
+ABCDEFG
+BABCDEF
+CBABCDE
+DCBABCD
+EDCBABC
+数据规模与约定
+1 <= n, m <= 26。
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+    int n, m;
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cout << char('A' + abs(i - j));
+        }
+        cout << endl;
+    }
+    return 0;
+}
