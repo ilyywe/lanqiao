7f2434adc98e42d2f1f14180dbe87085e78bf95a
陈欣 <95323362@qq.com>
2016-05-19 22:37:16 +0800

    -

---
+++ 算法提高/算法提高 2-1屏幕打印.cpp
@@ -0,0 +1,12 @@
+算法提高 2-1屏幕打印
+样例输出
+**********************
+* My first C program *
+**********************
+
+#include <iostream>
+using namespace std;
+int main() {
+	cout << "**********************\n* My first C program *\n**********************";
+	return 0;
+}
