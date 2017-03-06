c75b26a0f7c2900f1da4c95763ee4f94001d1cf2
liuchuo <95323362@qq.com>
2017-03-06 14:38:59 +0800

    -

--- 算法提高/ADV-105 算法提高 不同单词个数统计 .cpp
+++ 算法提高/ADV-105 算法提高 不同单词个数统计 .cpp
@@ -1,4 +1,4 @@
-算法提高 不同单词个数统计 
+ADV-105 算法提高 不同单词个数统计 
 问题描述
 　　编写一个程序，输入一个句子，然后统计出这个句子当中不同的单词个数。例如：对于句子“one little two little three little boys”，总共有5个不同的单词：one, little, two, three, boys。
 　　说明：（1）由于句子当中包含有空格，所以应该用gets函数来输入这个句子；（2）输入的句子当中只包含英文字符和空格，单词之间用一个空格隔开；（3）不用考虑单词的大小写，假设输入的都是小写字符；（4）句子长度不超过100个字符。
@@ -14,11 +14,9 @@ one little two little three little boys
 #include <set>
 using namespace std;
 int main() {
-	string t;
-	set<string> s;
-	while(cin >> t) {
-		s.insert(t);
-	}
-	cout << s.size();
-	return 0;
+    string t;
+    set<string> s;
+    while(cin >> t) s.insert(t);
+    cout << s.size();
+    return 0;
 }
