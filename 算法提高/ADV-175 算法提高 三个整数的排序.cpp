8bdc4c792fc634fdd16884916cbff61a2a36badb
liuchuo <95323362@qq.com>
2017-03-07 11:08:38 +0800

    -

--- 算法提高/ADV-175 算法提高 三个整数的排序.cpp
+++ 算法提高/ADV-175 算法提高 三个整数的排序.cpp
@@ -1,6 +1,4 @@
-//算法提高 三个整数的排序  
-时间限制：1.0s   内存限制：256.0MB
-提交此题    
+ADV-175 算法提高 三个整数的排序   
 问题描述
 　　﻿输入三个数，比较其大小，并从大到小输出。
 输入格式
@@ -17,21 +15,12 @@ using namespace std;
 int main() {
     int a, b, c, t;
     cin >> a >> b >> c;
-    if (a < b) {
-        t = a;
-        a = b;
-        b = t;
-    }
-    if (b < c) {
-        t = b;
-        b = c;
-        c = t;
-    }
-    if (a < b) {
-        t = a;
-        a = b;
-        b = t;
-    }
+    if (a < b)
+        swap(a, b);
+    if (b < c)
+        swap(b, c);
+    if (a < b)
+        swap(a, b);
     cout << a << " " << b << " " << c;
     return 0;
 }
