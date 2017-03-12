ff2c91ef2166f2e966591d58facd2a47df1b13f8
liuchuo <95323362@qq.com>
2017-03-13 05:32:40 +0800

    -

--- 算法提高/ADV-20 算法提高 交换Easy.cpp
+++ 算法提高/ADV-20 算法提高 交换Easy.cpp
@@ -1,4 +1,4 @@
-算法提高 交换Easy
+ADV-20 算法提高 交换Easy
 问题描述
 　　给定N个整数组成的序列，每次交换当前第x个与第y个整数，要求输出最终的序列。
 输入格式
@@ -22,19 +22,18 @@
 #include <iostream>
 using namespace std;
 int main() {
-	int n, m;
-	cin >> n >> m;
-	int *a = new int[n];
-	for(int i = 0; i < n; i++) {
-		cin >> a[i];
-	}
-	for(int i = 0; i < m; i++) {
-		int p, q;
-		cin >> p >> q;
-		swap(a[p-1], a[q-1]);
-	}
-	for(int i = 0; i < n; i++) {
-		cout << a[i] << endl;
-	}
-	return 0;
+    int n, m;
+    cin >> n >> m;
+    int *a = new int[n];
+    for(int i = 0; i < n; i++)
+        cin >> a[i];
+    for(int i = 0; i < m; i++) {
+        int p, q;
+        cin >> p >> q;
+        swap(a[p-1], a[q-1]);
+    }
+    for(int i = 0; i < n; i++)
+        cout << a[i] << endl;
+    delete [] a;
+    return 0;
 }
