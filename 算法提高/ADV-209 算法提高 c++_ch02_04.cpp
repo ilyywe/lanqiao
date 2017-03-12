b840d0e967a104be9e4d7418e313d15c8c3d7b70
liuchuo <95323362@qq.com>
2017-03-13 05:32:47 +0800

    -

--- 算法提高/ADV-209 算法提高 c++_ch02_04.cpp
+++ 算法提高/ADV-209 算法提高 c++_ch02_04.cpp
@@ -1,4 +1,4 @@
-算法提高 c++_ch02_04
+ADV-209 算法提高 c++_ch02_04
 问题描述
 　　输出1~100间的质数并显示出来。注意1不是质数。
 输出格式
@@ -11,20 +11,19 @@
 #include <iostream>
 using namespace std;
 bool isprime(int n) {
-	if(n <= 1)
-		return false;
-	for(int i = 2; i * i <= n; i++) {
-		if(n % i == 0)
-			return false;
-	}
-	return true;
+    if(n <= 1)
+        return false;
+    for(int i = 2; i * i <= n; i++) {
+        if(n % i == 0)
+            return false;
+    }
+    return true;
 }
 
 int main() {
-	for(int i = 1; i <= 100; i++) {
-		if(isprime(i)) {
-			cout << i << endl;
-		}
-	}
-	return 0;
+    for(int i = 1; i <= 100; i++) {
+        if(isprime(i))
+            cout << i << endl;
+    }
+    return 0;
 }
