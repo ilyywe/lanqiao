6c0bc66dc5b01a7f4860487da9e1b05bce4b0708
liuchuo <95323362@qq.com>
2017-03-14 22:32:59 +0800

    -

--- 算法提高/ADV-210 算法提高 2-1屏幕打印.cpp
+++ 算法提高/ADV-210 算法提高 2-1屏幕打印.cpp
@@ -1,4 +1,4 @@
-算法提高 2-1屏幕打印
+ADV-210 算法提高 2-1屏幕打印
 样例输出
 **********************
 * My first C program *
@@ -7,6 +7,6 @@
 #include <iostream>
 using namespace std;
 int main() {
-	cout << "**********************\n* My first C program *\n**********************";
-	return 0;
+    cout << "**********************\n* My first C program *\n**********************";
+    return 0;
 }
