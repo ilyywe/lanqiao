20781b687161147c095739bd2010d46002c575a6
liuchuo <95323362@qq.com>
2017-03-15 16:53:40 +0800

    -

--- 算法提高/ADV-211 算法提高 2-2整数求和.cpp
+++ 算法提高/ADV-211 算法提高 2-2整数求和.cpp
@@ -1,4 +1,4 @@
-算法提高 2-2整数求和
+ADV-211 算法提高 2-2整数求和
 基于例子3 ，写一个程序，实现整数求和:
 样例输入
 3 4
@@ -8,8 +8,8 @@
 #include <iostream>
 using namespace std;
 int main() {
-	int a, b;
-	cin >> a >> b;
-	cout << a + b;
-	return 0;
+    int a, b;
+    cin >> a >> b;
+    cout << a + b;
+    return 0;
 }
