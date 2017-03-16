26af8a6ab41d1337b39651f0250b7d9603dc9916
liuchuo <95323362@qq.com>
2017-03-16 20:50:50 +0800

    ·

--- 算法提高/ADV-70 算法提高 冒泡法排序.cpp
+++ 算法提高/ADV-70 算法提高 冒泡法排序.cpp
@@ -1,4 +1,4 @@
-算法提高 冒泡法排序
+ADV-70 算法提高 冒泡法排序
 输入10个数，用“冒泡法”对10个数排序（由小到大）这10个数字在100以内。
 样例输入
 1 3 6 8 2 7 9 0 4 5
@@ -8,19 +8,19 @@
 #include <iostream>
 using namespace std;
 int main() {
-	int a[10];
-	for(int i = 0; i < 10; i++) {
-		cin >> a[i];
-	}
-	for(int i = 0; i < 9; i++) {
-		for(int j = 0; j < 9 - i; j++) {
-			if(a[j] > a[j+1]) {
-				swap(a[j], a[j+1]);
-			}
-		}
-	}
-	for(int i = 0; i < 10; i++) {
-		cout << a[i] << " ";
-	}
-	return 0;
+    int a[10];
+    for(int i = 0; i < 10; i++) {
+        cin >> a[i];
+    }
+    for(int i = 0; i < 9; i++) {
+        for(int j = 0; j < 9 - i; j++) {
+            if(a[j] > a[j+1]) {
+                swap(a[j], a[j+1]);
+            }
+        }
+    }
+    for(int i = 0; i < 10; i++) {
+        cout << a[i] << " ";
+    }
+    return 0;
 }
