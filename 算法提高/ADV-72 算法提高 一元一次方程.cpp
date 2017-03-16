bb9e060a5a9e8fea6f5a81d8671825f719d6822d
liuchuo <95323362@qq.com>
2017-03-16 20:51:25 +0800

    ·

--- 算法提高/ADV-72 算法提高 一元一次方程.cpp
+++ 算法提高/ADV-72 算法提高 一元一次方程.cpp
@@ -1,4 +1,4 @@
-算法提高 一元一次方程
+ADV-72 算法提高 一元一次方程
 　　输入一元一次方法的ax+b=0的解。且数据均在double类型以内,且一定有解（保留2位小数）
 样例输入
 2 6
@@ -8,10 +8,10 @@
 #include <iostream>
 using namespace std;
 int main() {
-	double ans;
-	double a, b;
-	cin >> a >> b;
-	ans = (0 - b) / a;
-	printf("%.2lf", ans);
-	return 0;
+    double ans;
+    double a, b;
+    cin >> a >> b;
+    ans = (0 - b) / a;
+    printf("%.2lf", ans);
+    return 0;
 }
