251a3b23617492cc07de161c129089f53929c67d
liuchuo <95323362@qq.com>
2017-03-18 00:47:48 +0800

    -

--- 算法提高/ADV-77 算法提高 统计平均成绩.cpp
+++ 算法提高/ADV-77 算法提高 统计平均成绩.cpp
@@ -1,4 +1,4 @@
-算法提高 统计平均成绩
+ADV-77 算法提高 统计平均成绩
 有4个学生，上4门课，要求输入全部学生的各门课成绩，并分别求出每门课的平均成绩。(保留2位小数)
 　　括号里是解释内容，不用输入输出。输入的所有数都为0到100之间（包括端点）的整数
 样例输入
@@ -16,15 +16,15 @@
 #include <cstdio>
 using namespace std;
 int main() {
-	double c1 = 0.0, c2 = 0.0, c3 = 0.0, c4 = 0.0;
-	double t1, t2, t3, t4;
-	for(int i = 0; i < 4; i++) {
-		cin >> t1 >> t2 >> t3 >> t4;
-		c1 += t1;
-		c2 += t2;
-		c3 += t3;
-		c4 += t4;
-	}
-	printf("%.2lf\n%.2lf\n%.2lf\n%.2lf", c1 / 4, c2 / 4, c3 / 4, c4 / 4);
-	return 0;
+    double c1 = 0.0, c2 = 0.0, c3 = 0.0, c4 = 0.0;
+    double t1, t2, t3, t4;
+    for(int i = 0; i < 4; i++) {
+        cin >> t1 >> t2 >> t3 >> t4;
+        c1 += t1;
+        c2 += t2;
+        c3 += t3;
+        c4 += t4;
+    }
+    printf("%.2lf\n%.2lf\n%.2lf\n%.2lf", c1 / 4, c2 / 4, c3 / 4, c4 / 4);
+    return 0;
 }
