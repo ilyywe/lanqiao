bea926e59aa0e6ae30094ec6e3449d50e2c3fee3
liuchuo <95323362@qq.com>
2017-03-18 00:48:59 +0800

    1

--- 算法提高/ADV-80 算法提高 选最大数.cpp
+++ 算法提高/ADV-80 算法提高 选最大数.cpp
@@ -1,4 +1,4 @@
-算法提高 选最大数
+ADV-80 算法提高 选最大数
 输入3个整数a、b、c，（数的范围是[1,10000]）输出其中最大的数。(用指针实现)
 样例输入
 2 5 1
@@ -8,12 +8,12 @@
 #include <iostream>
 using namespace std;
 int main() {
-	int a, b, c;
-	cin >> a >> b >> c;
-	int *p = a;
-	if(*p < b)
-		p = &b;
-	if(*p < c)
-		p = &c;
-	return 0;
+    int a, b, c;
+    cin >> a >> b >> c;
+    int *p = a;
+    if(*p < b)
+        p = &b;
+    if(*p < c)
+        p = &c;
+    return 0;
 }
