436b764e8cc6c1504101d5fd5a79df998a0ceca3
liuchuo <95323362@qq.com>
2017-03-21 09:43:22 +0800

    -

--- 算法提高/ADV-89 算法提高 输出九九乘法表.cpp
+++ 算法提高/ADV-89 算法提高 输出九九乘法表.cpp
@@ -1,4 +1,4 @@
- 算法提高 输出九九乘法表
+ADV-89 算法提高 输出九九乘法表
  编制程序，按照下述格式打印九九乘法表。
 输出样例：
 Nine-by-nine Multiplication Table
@@ -20,12 +20,12 @@ Nine-by-nine Multiplication Table
 #include <iostream>
 using namespace std;
 int main() {
-	cout << "Nine-by-nine Multiplication Table" << endl;
-	cout << "--------------------------------------" << endl;
-	cout << "     1   2   3   4   5   6   7   8   9" << endl;
-	cout << "--------------------------------------" << endl;
-	cout << " 1   1\n2   2   4\n3   3   6   9\n4   4   8  12  16\n5   5  10  15  20  25\n6   6  12  18  24  30  36\n";
-	cout << "7   7  14  21  28  35  42  49\n8   8  16  24  32  40  48  56  64\n9   9  18  27  36  45  54  63  72  81\n";
-	cout << "--------------------------------------";
-	return 0;
+    cout << "Nine-by-nine Multiplication Table" << endl;
+    cout << "--------------------------------------" << endl;
+    cout << "     1   2   3   4   5   6   7   8   9" << endl;
+    cout << "--------------------------------------" << endl;
+    cout << " 1   1\n2   2   4\n3   3   6   9\n4   4   8  12  16\n5   5  10  15  20  25\n6   6  12  18  24  30  36\n";
+    cout << "7   7  14  21  28  35  42  49\n8   8  16  24  32  40  48  56  64\n9   9  18  27  36  45  54  63  72  81\n";
+    cout << "--------------------------------------";
+    return 0;
 }
