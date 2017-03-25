fc6ff20816d58f750cde349be07e7ac0f8255962
liuchuo <95323362@qq.com>
2017-03-25 10:59:08 +0800

    -

--- 算法提高/ADV-96 算法提高 复数求和.cpp
+++ 算法提高/ADV-96 算法提高 复数求和.cpp
@@ -1,4 +1,4 @@
-算法提高 复数求和
+ADV-96 算法提高 复数求和
 从键盘读入n个复数（实部和虚部都为整数）用链表存储，遍历链表求出n个复数的和并输出。
 样例输入: 
 3 
@@ -22,20 +22,20 @@
 #include <iostream>
 using namespace std;
 int main() {
-	int a = 0, b = 0;
-	int n;
-	cin >> n;
-	for(int i = 0; i < n; i++) {
-		int ta, tb;
-		cin >> ta >> tb;
-		a += ta;
-		b += tb;
-	}
-	cout << a;
-	if(b >= 0) {
-		cout << "+" << b << "i";
-	} else if(b < 0) {
-		cout << b << "i";
-	}
-	return 0;
+    int a = 0, b = 0;
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        int ta, tb;
+        cin >> ta >> tb;
+        a += ta;
+        b += tb;
+    }
+    cout << a;
+    if(b >= 0) {
+        cout << "+" << b << "i";
+    } else if(b < 0) {
+        cout << b << "i";
+    }
+    return 0;
 }
