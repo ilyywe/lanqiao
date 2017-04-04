75a606c02e38c4c20380c32d2f4c4b94dd1291d2
liuchuo <95323362@qq.com>
2017-04-04 11:29:18 +0800

    -

--- 算法训练/ALGO-111 算法训练 明明的随机数.cpp
+++ 算法训练/ALGO-111 算法训练 明明的随机数.cpp
@@ -1,4 +1,4 @@
-算法训练 明明的随机数 
+ALGO-111 算法训练 明明的随机数 
 问题描述
 　　明明想在学校中请一些同学一起做一项问卷调查，为了实验的客观性，他先用计算机生成了N个1到1000之间的随机整数（N≤100），对于其中重复的数字，只保留一个，把其余相同的数去掉，不同的数对应着不同的学生的学号。然后再把这些数从小到大排序，按照排好的顺序去找同学做调查。请你协助明明完成“去重”与“排序”的工作。
 输入格式
@@ -20,16 +20,16 @@
 #include <set>
 using namespace std;
 int main() {
-	set<int> s;
-	int n;
-	cin >> n;
-	for(int i = 0; i < n; i++) {
-		int temp;
-		cin >> temp;
-		s.insert(temp);
-	}
-	cout << s.size() << endl;
-	for(set<int>::iterator it = s.begin(); it != s.end(); it++) {
-		cout << *it << " ";
-	}
+    set<int> s;
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        int temp;
+        cin >> temp;
+        s.insert(temp);
+    }
+    cout << s.size() << endl;
+    for(set<int>::iterator it = s.begin(); it != s.end(); it++) {
+        cout << *it << " ";
+    }
 }
