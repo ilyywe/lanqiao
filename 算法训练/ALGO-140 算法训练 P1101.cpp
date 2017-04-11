2d5c6adf6c2337d7bbcbcd889e657a24dc97a946
liuchuo <95323362@qq.com>
2017-04-11 08:52:13 +0800

    8

--- 算法训练/ALGO-140 算法训练 P1101.cpp
+++ 算法训练/ALGO-140 算法训练 P1101.cpp
@@ -1,4 +1,4 @@
-算法训练 P1101
+ALGO-140 算法训练 P1101
 有一份提货单，其数据项目有：商品名（MC）、单价（DJ）、数量（SL）。定义一个结构体prut，其成员是上面的三项数据。在主函数中定义一个prut类型的结构体数组，输入每个元素的值，计算并输出提货单的总金额。
 　　输入格式：第一行是数据项个数N(N<100)，接下来每一行是一个数据项。商品名是长度不超过100的字符串，单价为double类型，数量为整型。
 　　输出格式：double类型的总金额。
@@ -14,6 +14,7 @@
 
 #include <iostream>
 #include <cstdio>
+#include <vector>
 using namespace std;
 struct prut{
     string MC;
@@ -24,7 +25,7 @@ int main() {
     int N;
     cin >> N;
     double sum = 0.0;
-    struct prut *a = new struct prut [N];
+    vector<prut> a(N);
     for(int i = 0; i < N; i++) {
         cin >> a[i].MC >> a[i].DJ >> a[i].SL;
         sum += a[i].DJ * a[i].SL;
