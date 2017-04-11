0529e976b6e4761b6a0effdf2103991bd066f499
liuchuo <95323362@qq.com>
2017-04-11 08:52:04 +0800

    6

--- 算法训练/ALGO-129 算法训练 特殊的数字四十.cpp
+++ 算法训练/ALGO-129 算法训练 特殊的数字四十.cpp
@@ -1,4 +1,4 @@
-算法训练 特殊的数字四十
+ALGO-129 算法训练 特殊的数字四十
 　特殊的数字四十
 问题描述
 　　1234是一个非常特殊的四位数，因为它的各位数之和为10，编程求所有这样的四位十进制数。
@@ -8,14 +8,14 @@
 #include <iostream>
 using namespace std;
 int main() {
-	for(int i = 1; i <= 9; i++) {
-		for(int j = 0; j <= 9; j++) {
-			for(int k = 0; k <= 9; k++) {
-				int l = 10 - i - j - k;
-				if(l >= 0 && l <= 9)
-					cout << i << j << k << l << endl;
-			}
-		}
-	}
-	return 0;
+    for(int i = 1; i <= 9; i++) {
+        for(int j = 0; j <= 9; j++) {
+            for(int k = 0; k <= 9; k++) {
+                int l = 10 - i - j - k;
+                if(l >= 0 && l <= 9)
+                    cout << i << j << k << l << endl;
+            }
+        }
+    }
+    return 0;
 }
