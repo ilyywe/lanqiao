dca33fc3ab8053875661af489d3b1e959870418f
liuchuo <95323362@qq.com>
2017-04-17 12:03:05 +0800

    -

--- 算法训练/ALGO-39 算法训练 数组排序去重.cpp
+++ 算法训练/ALGO-39 算法训练 数组排序去重.cpp
@@ -1,4 +1,4 @@
-算法训练 数组排序去重 
+ALGO-39 算法训练 数组排序去重 
 问题描述
 　　输入10个整数组成的序列，要求对其进行升序排序，并去掉重复元素。
 输入格式
@@ -18,16 +18,16 @@
 #include <algorithm>
 using namespace std;
 int main() {
-	int a[10];
-	for(int i = 0; i < 10; i++) {
-		cin >> a[i];
-	}
-	sort(a, a+10);
-	cout << a[0] << endl;
-	for(int i = 1; i < 10; i++) {
-		if(a[i] != a[i-1]) {
-			cout << a[i] << endl;
-		}
-	}
-	return 0;
+    int a[10];
+    for(int i = 0; i < 10; i++) {
+        cin >> a[i];
+    }
+    sort(a, a+10);
+    cout << a[0] << endl;
+    for(int i = 1; i < 10; i++) {
+        if(a[i] != a[i-1]) {
+            cout << a[i] << endl;
+        }
+    }
+    return 0;
 }
