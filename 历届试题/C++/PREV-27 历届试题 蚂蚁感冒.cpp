d0817a1795cf21165bb039d7caa4322fea594cec
liuchuo <95323362@qq.com>
2018-11-28 17:05:26 +0800

    Create PREV-27 历届试题 蚂蚁感冒.cpp

---
+++ 历届试题/C++/PREV-27 历届试题 蚂蚁感冒.cpp
@@ -0,0 +1,29 @@
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+    int n, cold, t, ll = 0, lr = 0, rl = 0, rr = 0, ans = 0;
+    cin >> n >> cold;
+    for (int i = 0; i < n - 1; i++) {
+        cin >> t;
+        if (t > 0) {
+            if (abs(t) < abs(cold)) lr++;
+            else rr++;
+        } else {
+            if (abs(t) < abs(cold)) ll++;
+            else rl++;
+        }
+    }
+    if (cold < 0) {
+        if (lr > 0) ans = lr + rl + 1;
+        else ans = 1;
+
+    } else {
+        if (rl > 0) ans = rl + lr + 1;
+        else ans = 1;
+
+    }
+    cout << ans;
+    return 0;
+}
