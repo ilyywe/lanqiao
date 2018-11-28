863d439014998a5584ffd7996f040d1d1ab2c8d5
liuchuo <95323362@qq.com>
2018-11-29 00:37:47 +0800

    Create PREV-6 历届试题 翻硬币.cpp

---
+++ 历届试题/C++/PREV-6 历届试题 翻硬币.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    int cnt = 0;
+    string s1, s2;
+    cin >> s1 >> s2;
+    for (int i = 0; i < s1.length(); i++) {
+        if (s1[i] != s2[i]) {
+            if (s1[i] == 'o') s1[i] = '*';
+            else s1[i] = 'o';
+            if (s1[i + 1] == 'o') s1[i + 1] = '*';
+            else s1[i + 1] = 'o';
+            cnt++;
+        }
+    }
+    cout << cnt;
+    return 0;
+}
