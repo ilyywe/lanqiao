d71482c640ada92af0a2a3f91f88142b3ea02458
liuchuo <95323362@qq.com>
2018-11-30 08:55:45 +0800

    Create PREV-8 历届试题 买不到的数目.cpp

---
+++ 历届试题/C++/PREV-8 历届试题 买不到的数目.cpp
@@ -0,0 +1,27 @@
+
+#include <iostream>
+#include <cmath>
+#include <vector>
+using namespace std;
+int main() {
+    int t, n = 0, dp[100001] = {0}, maxn = 100000;
+    vector<int> w(1), v(1);
+    while (cin >> t) {
+        w.push_back(t);
+        v.push_back(t);
+        n++;
+    }
+    for (int i = 1; i <= n; i++) {
+        for (int j = w[i]; j <= maxn; j++) {
+            dp[j] = max(dp[j], dp[j - w[i]] + v[i]);
+        }
+    }
+    dp[0] = -1;
+    for (int i = maxn; i >= 0; i--) {
+        if (dp[i] < i) {
+            cout << i;
+            return 0;
+        }
+    }
+    return 0;
+}
