f07872abf43233471fc7e611d53b37048c7a3bab
liuchuo <95323362@qq.com>
2018-12-05 08:42:26 +0800

    Create ADV-113 算法提高 c++_ch02_02.cpp

---
+++ 算法提高/C++/ADV-113 算法提高 c++_ch02_02.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b, ans;
+    char c;
+    cin >> a >> b >> c;
+    if (c == '+') ans = a + b;
+    else if (c == '-') ans = a - b;
+    else if (c == '*') ans = a * b;
+    else if (c == '%') ans = a % b;
+    else if (c == '/') ans = a / b;
+    cout << ans;
+    return 0;
+}
