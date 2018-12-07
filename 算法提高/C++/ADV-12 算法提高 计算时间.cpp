954f930b3ed16bb5aa431ac757d846cef5433f38
liuchuo <95323362@qq.com>
2018-12-07 19:08:11 +0800

    Update ADV-12 算法提高 计算时间.cpp

--- 算法提高/C++/ADV-12 算法提高 计算时间.cpp
+++ 算法提高/C++/ADV-12 算法提高 计算时间.cpp
@@ -1,18 +1,15 @@
+
 #include <iostream>
-#include <cstdio>
 using namespace std;
 int main() {
-    int n;
-    cin >> n;
-    for(int i = 0; i < n; i++) {
-        int t;
-        cin >> t;
-        int h, m;
-        h = t / 3600;
-        t = t % 3600;
-        m = t / 60;
-        t = t % 60;
-        printf("%02d:%02d:%02d\n", h, m, t);
+    int k, n;
+    cin >> k;
+    for (int i = 0; i < k; i++) {
+        cin >> n;
+        int s = n % 60;
+        int m = (n / 60) % 60;
+        int h = n / (60 * 60);
+        printf("%02d:%02d:%02d\n", h, m, s);
     }
     return 0;
 }
