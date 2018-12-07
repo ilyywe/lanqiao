0ec7433fa8c93df30db28132afc60ebd5e674136
liuchuo <95323362@qq.com>
2018-12-07 19:08:06 +0800

    Create ADV-117 算法提高 进制转换.cpp

---
+++ 算法提高/C++/ADV-117 算法提高 进制转换.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+using namespace std;
+int main() {
+    int n = 0;
+    char c1, c2, c3;
+    string s(3, '0'), ans8;
+    scanf("%c %c %c", &c1, &c2, &c3);
+    s[0] = c1;
+    s[1] = c2;
+    s[2] = c3;
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] >= '0' && s[i] <= '9') n = n * 16 + s[i] - '0';
+        else n = n * 16 + s[i] - 'A' + 10;
+    }
+    if (s[0] == '0') s = s.substr(1, s.length() - 1);
+    if (s[0] == '0') s = s.substr(1, s.length() - 1);
+    cout << s << " " << n << " ";
+    while (n) {
+        ans8 = char(n % 8 + '0') + ans8;
+        n /= 8;
+    }
+    if (ans8 == "") ans8 = "0";
+    cout << ans8;
+    return 0;
+}
