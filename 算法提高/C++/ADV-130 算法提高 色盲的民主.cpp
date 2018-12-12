a6031cd8762c908f190c154de307292d1297ea45
liuchuo <95323362@qq.com>
2018-12-12 18:02:41 +0800

    Create ADV-130 算法提高 色盲的民主.cpp

---
+++ 算法提高/C++/ADV-130 算法提高 色盲的民主.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+#include <map>
+#include <cmath>
+using namespace std;
+int main() {
+    int n, maxn = 0;
+    string s;
+    map<string, int> m;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        getline(cin,s);
+        m[s]++;
+        maxn = max(maxn, m[s]);
+    }
+    for (map<string, int>::iterator i = m.begin(); i != m.end(); i++) {
+        if (i->second == maxn)
+            cout << i->first << endl;
+    }
+    return 0;
+}
