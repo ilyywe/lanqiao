20d95c629172369eb472994084e6d76f4f76840f
liuchuo <95323362@qq.com>
2018-12-25 01:24:58 +0800

    Update ADV-135 算法提高 三角形面积.cpp

--- 算法提高/C++/ADV-135 算法提高 三角形面积.cpp
+++ 算法提高/C++/ADV-135 算法提高 三角形面积.cpp
@@ -1,12 +1,11 @@
+
 #include <iostream>
-#include <cstdio>
 #include <cmath>
 using namespace std;
 int main() {
     int a, b, c;
     cin >> a >> b >> c;
-    double p = (a + b + c) / 2.0;
-    double area = sqrt(p * (p - a) * (p - b) * (p - c));
-    printf("%.2f", area);
+    double s = (a + b + c) / 2.0;
+    printf("%.2f", sqrt(s * (s - a) * (s - b) * (s - c)));
     return 0;
 }
