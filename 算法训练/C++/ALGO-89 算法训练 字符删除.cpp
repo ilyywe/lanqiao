d3d703b97b3b976491afd6b2e3bf131c53d85d54
liuchuo <95323362@qq.com>
2019-01-11 01:17:58 +0800

    :tada: feat: initial commit for algo 89

---
+++ 算法训练/C++/ALGO-89 算法训练 字符删除.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+using namespace std;
+int main() {
+    string s;
+    char c;
+    getline(cin, s);
+    cin >> c;
+    for (int i = 0; i < s.size(); i++)
+        if (s[i] != c) cout << s[i];
+    cout << endl;
+    return 0;
+}
