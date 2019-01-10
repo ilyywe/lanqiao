77a0da59e8707b42d813167af49dcd5181c26122
liuchuo <95323362@qq.com>
2019-01-11 01:12:26 +0800

    :tada: feat: initial commit

---
+++ 算法提高/C++/ADV-14 算法提高 卡勒沃夫之弱水路三千(提高型).cpp
@@ -0,0 +1,40 @@
+
+#include <iostream>
+#include <algorithm>
+#include <set>
+#include <map>
+using namespace std;
+int main() {
+    int k, n;
+    cin >> k;
+    while (k--) {
+        string ans = "";
+        cin >> n;
+        map<string, set<string> > m;
+        map<string, int> vis;
+        for (int i = 0; i < n; i++) {
+            string s1, s2;
+            cin >> s1 >> s2;
+            m[s1];
+            m[s2].insert(s1);
+        }
+        while (1) {
+            int check = 0;
+            for (map<string, set<string> >::iterator i = m.begin(); i != m.end(); i++) {
+                string girl = i->first;
+                if (vis[girl] == 0 && m[girl].size() == 0) {
+                    check = 1;
+                    vis[girl] = 1;
+                    for (map<string, set<string> >::iterator j = m.begin(); j != m.end(); j++) {
+                        j->second.erase(girl);
+                    }
+                    ans = ans + girl + " ";
+                    break;
+                }
+            }
+            if (check == 0) break;
+        }
+        cout << ans.substr(0, ans.length() - 1) << endl;
+    }
+    return 0;
+}
---
+++ 算法提高/C++/ADV-140 算法提高 开灯游戏.cpp
@@ -0,0 +1,34 @@
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+void f(string s) {
+    int a[11], sum = 0;
+    memset(a, -1, sizeof(a));
+    if (s[0] == '1') a[2] *= -1, a[4] *= -1;
+    if (s[1] == '1') a[1] *= -1, a[3] *= -1, a[5] *= -1;
+    if (s[2] == '1') a[2] *= -1, a[6] *= -1;
+    if (s[3] == '1') a[1] *= -1, a[5] *= -1, a[7] *= -1;
+    if (s[4] == '1') a[2] *= -1, a[4] *= -1, a[6] *= -1, a[8] *= -1;
+    if (s[5] == '1') a[3] *= -1, a[5] *= -1, a[9] *= -1;
+    if (s[6] == '1') a[4] *= -1, a[8] *= -1;
+    if (s[7] == '1') a[5] *= -1, a[7] *= -1, a[9] *= -1;
+    if (s[8] == '1') a[6] *= -1, a[8] *= -1;
+    for (int i = 1; i <= 9; i++)
+        if (a[i] == 1) sum++;
+    if (sum == 4) cout << s << endl;
+}
+int main() {
+    for (int i = 0; i < 512; i++) {
+        int n = i;
+        string s;
+        while (n) {
+            s = (char) (n % 2 + '0') + s;
+            n /= 2;
+        }
+        string t(9 - s.length(), '0');
+        s = t + s;
+        f(s);
+    }
+    return 0;
+}
