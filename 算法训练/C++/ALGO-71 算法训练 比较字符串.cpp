a8327f023c547e88d7c4ed35cb37fd1b571ce4d9
liuchuo <95323362@qq.com>
2019-01-11 01:18:23 +0800

    :tada: feat: initial commit for algo 71

---
+++ 算法训练/C++/ALGO-71 算法训练 比较字符串.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string s1, s2;
+    cin >> s1 >> s2;
+    for (int i = 0; i < s1.length() && i < s2.length(); i++) {
+        if (s1[i] != s2[i]) {
+            cout << s1[i] - s2[i] << endl;
+            return 0;
+        }
+    }
+    cout << 0 << endl;
+    return 0;
+}
