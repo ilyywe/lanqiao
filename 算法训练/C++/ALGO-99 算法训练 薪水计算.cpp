6576f82f5b9cc182dfbb61e45d10285d4067324f
liuchuo <95323362@qq.com>
2019-01-11 01:16:01 +0800

    :tada: feat: initial commit for algo 99

---
+++ 算法训练/C++/ALGO-99 算法训练 薪水计算.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+    int n;
+    double sal, sum;
+    cin >> n >> sal;
+    sum = min(40, n) * sal + min(10, max(0, n - 40)) * sal * 1.5 + max(0, n - 50) * sal * 2;
+    printf("%.2f",sum);
+    return 0;
+}
